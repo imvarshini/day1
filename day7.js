1792a2f2355164a36ecfb88b68b61daf011979b8
imvarshini <94341830+imvarshini@users.noreply.github.com>
2021-11-29 18:12:05 +0530

    Add files via upload

---
+++ day7.js
@@ -0,0 +1,32 @@
+let pi="3.14";
+
+function firstName (){
+    var pi=22;
+    console.log(pi);
+    if(pi){
+        const pi=33;
+        console.log(pi);
+
+    }
+    if(pi){
+        let pi=44;
+        pi++;
+        console.log(pi);
+    }
+}
+firstName();
+console.log(pi);
+
+let fname=(fname) =>{
+    console.log(fname);
+    console.log("shankar");
+};
+fname("jay");
+
+
+let fname1="jay";
+let lname1="vijay";
+console.log(`this is name :${fname1} ${lname1}`);
+
+
+
---
+++ example.js
@@ -0,0 +1,23 @@
+var cont=document.getElementById("exp");
+var button=document.getElementById("btn");
+button.addEventListener("click",function () {
+    console.log("hello all");
+});
+console.log(cont);
+var container=document.getElementsByClassName("example");
+var but=document.getElementById("button");
+but.addEventListener("click", function() {
+    console.log("Good Morning");
+});
+console.log(container);
+var con=document.getElementsByName("forexample");
+var butt=document.getElementById("butexp");
+but.addEventListener("click", function() {
+    console.log("Good Evening");
+});
+console.log(con);
+var tagg=document.getElementsByTagName("p");
+var neww=document.getElementById("tag");
+but.addEventListener("click", function() {
+    console.log("Welcome You All");
+});
